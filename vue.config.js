fd25fe3c6a68a3fab708ee27d91dc30b414b11f2
Joshua Smallwood <31932412+smallwoj@users.noreply.github.com>
2021-07-02 18:46:32 -0500

    Update public path & package lock

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  publicPath: process.env.NODE_ENV === 'production' ? '/smallwoj.github.io/' : '/'
+  publicPath: '/smallwoj.github.io/'
 }
