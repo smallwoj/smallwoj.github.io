9649487c64fc4351419eb95182d5a693c22d4ba3
Joshua Smallwood <31932412+smallwoj@users.noreply.github.com>
2021-07-02 19:03:59 -0500

    Revert path change

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  publicPath: '/smallwoj.github.io/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/smallwoj.github.io/' : '/'
 }
